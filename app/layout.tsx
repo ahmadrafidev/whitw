ad1d02069bceae86fecab34e756a5b54719927fa
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-08-20 17:01:55 +0700

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
+import { DM_Sans } from "next/font/google";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const dm_sans = DM_Sans({ 
+  weight: '400',
+  subsets: ["latin"]
+});
 
 export const metadata: Metadata = {
   title: "WHITW",
@@ -15,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={dm_sans.className}>
+      <body>{children}</body>
     </html>
   );
 }
